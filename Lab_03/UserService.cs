6374a7f0e80fe93f3ae7dd32b4fccdcc6773d34a
Krzysztof Trytek <Krzysztof.Trytek@microsoft.wsei.edu.pl>
2023-03-15 13:54:38 +0100

    Dodaj pliki projektów.

---
+++ Lab_03/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Lab_03
+{
+    public class UserService
+    {
+        private Dictionary<string, string> _users = new Dictionary<string, string>();
+
+        public void Add(string connectionId, string username)
+        {
+            _users[username] = connectionId;
+        }
+
+        public void RemoveByName(string username)
+        {
+            _users.Remove(username);
+        }
+
+        public string? GetConnectionIdByName(string username)
+        {
+
+            return _users.TryGetValue(username, out var connectionId) ? connectionId : null;
+
+        }
+
+        public IEnumerable<(string ConnectionId, string Username)> GetAll()
+        {
+            foreach (var user in _users)
+            {
+                yield return (user.Value, user.Key);
+            }
+        }
+    }
+}
